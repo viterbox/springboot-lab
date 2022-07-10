c5efc9e8191fed301bc80493de16a04134cac813
Franco Antonio Martinez Viterbo <fam003x@email.wal-mart.com>
2022-07-10 13:15:14 -0500

    Greeting controller created

---
+++ 01-restservice/src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,20 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    
+    public static final String template = "Hello, %s!";
+    public final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+        return new Greeting(counter.getAndIncrement(), String.format(template, name));
+    }
+    
+}
